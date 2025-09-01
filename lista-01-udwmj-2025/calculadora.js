68a95af58fe41516b7c8f07259d191b3ddd5a8f6
Camila Geovana Da silva <camilagsilv2@gmail.com>
2025-09-01 20:50:16 -0300

    Update calculadora.js

---
+++ lista-01-udwmj-2025/calculadora.js
@@ -0,0 +1,21 @@
+let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+let numero2 = parseFloat(prompt("Digite o segundo número:"));
+
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+alert(
+  "Resultados:\n" +
+  "Soma: " + soma + "\n" +
+  "Subtração: " + subtracao + "\n" +
+  "Multiplicação: " + multiplicacao + "\n" +
+  "Divisão: " + divisao
+);
+
+console.log("Resultados:");
+console.log("Soma: " + soma);
+console.log("Subtração: " + subtracao);
+console.log("Multiplicação: " + multiplicacao);
+console.log("Divisão: " + divisao);
