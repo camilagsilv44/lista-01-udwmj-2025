f3632112cb06a3b78bb697102a89ef0125b7cf94
Camila Geovana Da silva <camilagsilv2@gmail.com>
2025-09-01 20:50:59 -0300

    Update codigo.php

---
+++ lista-01-udwmj-2025/codigo.php
@@ -0,0 +1,14 @@
+<?php
+$num1 = 10;
+$num2 = 5;
+
+$soma = $num1 + $num2;
+$subtracao = $num1 - $num2;
+$multiplicacao = $num1 * $num2;
+$divisao = $num1 / $num2;
+
+echo "Soma: $soma<br>";
+echo "Subtração: $subtracao<br>";
+echo "Multiplicação: $multiplicacao<br>";
+echo "Divisão: $divisao<br>";
+?>
